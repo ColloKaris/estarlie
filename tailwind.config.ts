15e1b4b1c6258cc69748850dc231d02f943f92e4
Collins Kariuki <collinsmunkary@gmail.com>
2025-10-03 23:04:48 +0300

    working astro

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./components/**/*.{astro,js,ts}",
+    "./layouts/**/*.{astro,js,ts}",
+    "./pages/**/*.{astro,js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
